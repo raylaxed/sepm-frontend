d079cf3666ed380dcd40e289a50256454a0ad6c7
raimundkohlprath <e12026665@student.tuwien.ac.at>
2025-02-21 19:52:19 +0100

    added backend url

--- src/app/global/globals.ts
+++ src/app/global/globals.ts
@@ -215,8 +215,8 @@ export class Globals {
     if (window.location.port === '4200') { // local `ng serve`, backend at localhost:8080
       return 'http://localhost:8080/api/v1';
     } else {
-      // assume deployed somewhere and backend is available at same host/port as frontend
-      return window.location.protocol + '//' + window.location.host + window.location.pathname + 'api/v1';
+      // Use deployed backend URL
+      return 'https://sepm-backend-6xd0.onrender.com/api/v1';
     }
   }
 }
